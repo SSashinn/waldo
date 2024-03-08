3b5c247c51ae156d448eb9c6987cd986b8d6a911
SSashinn <sayedayaan226@gmail.com>
2024-03-08 22:35:11 +0530

    created home page ui

--- client/src/Route.jsx
+++ client/src/Route.jsx
@@ -1,11 +1,21 @@
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 export default function Router(){
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/signup",
+      element: <Signup />
     },
   ]);
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import {Link} from 'react-router-dom';
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div id='start-container'>
+        <img src='waldo.png?url'id='waldo'/>
+        <h2>WHERE IS WALDO?</h2>
+        <button type="button" className='start-btn'>START NOW</button>
+        <div id='auth-link'>
+          <span>Want to save your highscore, then </span>
+          <Link to='/signup'><span>signup</span></Link>
+          <span> or </span>
+          <Link to='/login'><span>login</span></Link>
+        </div>
+        <div id='highscore'>
+          <h4>HIGHSCORE</h4>
+          <ul>
+            <li>Aaa</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
